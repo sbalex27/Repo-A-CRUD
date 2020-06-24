631ce09550d76cb66f0e68553b8855c43bca20c1
Alex Escobedo <alex2000.sabe@outlook.com>
2020-06-23 23:22:19 -0600

    Revert "Modificación de mensaje de acción de objeto"
    This reverts commit 862d3515279ab94481ccb36d15a376b342ec3ee8.

--- SalesApp Alpha 2/PremadeMessage.cs
+++ SalesApp Alpha 2/PremadeMessage.cs
@@ -27,8 +27,8 @@ namespace SalesApp_Alpha_2
         {
             if (Settings.Default.MessageForEachAction)
             {
-                _title = $"{affected} {arg}";
-                _caption = o.ToString();
+                _title = arg;
+                _caption = $"{affected} {arg}: {o}";
                 _buttons = MessageBoxButtons.OK;
                 _icon = MessageBoxIcon.Information;
                 Show();
